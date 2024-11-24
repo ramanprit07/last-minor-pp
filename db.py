53549d57690eb79877f49410f8ef93b01a9a3025
Ramanprit kaur <ramanpritkaur37@gmail.com>
2024-11-24 21:28:21 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,18 +1,18 @@
 import mysql.connector
 connection=None
-
-try:
-    connection= mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        db='user_database'
-    )
-    if connection.is_connected():
-        return connection
-except mysql.connector.Error as e:
-    print(f"Error: {e}")
-    return None
+def get_connection():
+    try:
+        connection= mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            db='user_database'
+        )
+        if connection.is_connected():
+            return connection
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        return None
     
 
 cursor= connection.cursor(buffered=True)
